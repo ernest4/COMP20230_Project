df18d0afb002b4994c837027c1ebccc88567ee87
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2018-04-20 11:51:44 +0100

    Added some tests and utility functions

---
+++ ernestas_monkevicius_14493758_project/utility.py
@@ -0,0 +1,129 @@
+'''
+Created on 15 Apr 2018
+
+@author: ernest
+'''
+
+import math
+
+class Graph:
+    '''This class models the graph data structure.'''
+    def __init__(self):
+        self.__vertices = {}
+        
+    def setVertex(self, v):
+        if not v in self.getVertices():
+            #create a vertex key with empty adjacency dict
+            self.__vertices[v] = {}
+        else: #Error, vertex v already exists.
+            return -1
+        
+    def setEdge(self, v1, v2, cost=None):
+        #adds 1 or more bidirectional connections with corresponding cost.
+        if cost is None: #Default cost is 1.
+            cost = [1 for _ in range(0, len(v2))]
+        for c, v in enumerate(v2):
+            if v not in self.__vertices[v1]:
+                self.__vertices[v1][v] = int(cost[c])
+                self.__vertices[v][v1] = int(cost[c])
+                
+    def getEdge(self, v1, v2):
+        return self.__vertices[v1][v2]
+            
+    def getNeighbours(self, x):
+        return self.__vertices[x]
+    
+    def getVertices(self):
+        return self.__vertices.keys()
+            
+    def __str__(self):
+        returnStr = ""
+        for v in self.__vertices.keys():
+            returnStr = "\n".join([returnStr,''.join([v,"->",str(self.__vertices[v])])])
+        return returnStr
+    
+def BFS(graph, rootNode):
+    '''Finds all the nodes in the graph, starting from rootNode.'''
+    to_visit = []
+    to_visit.append(rootNode)
+    
+    visited = []
+    
+    while to_visit:
+        current = to_visit.pop(0)
+        if not current in visited:
+            visited.append(current)
+        
+        for neighbour in graph.getNeighbours(current):
+            if not neighbour in visited:
+                to_visit.append(neighbour)
+        
+    return visited
+
+def optimalPath(graph, rootNode, targetNode):
+    '''Based on algorithm from Wikipedia: https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+    Finds the most optimal path from rootNode to targetNode.'''
+    
+    #This function takes us through the graph and finds the costs of reaching vertices in the graph
+    def dijkstra(graph, source, destination):
+        dist = {}
+        prev = {}
+        Q = []
+        
+        for vertex in graph.getVertices():
+            dist[vertex] = float('inf')
+            prev[vertex] = None
+            Q.append(vertex)
+            
+        dist[source] = 0
+        
+        while Q:
+            u = Q[0] #start with the first element
+            for vertex in Q: #compare all vertices in Q and select the one with least distance
+                if dist[vertex] < dist[u]:
+                    u = vertex
+                    if u == destination: #we reached the destination node and can exit prematurely to save time
+                        return dist, prev
+                    
+            Q.remove(u) #remove the visited vertex
+            
+            for v in graph.getNeighbours(u):
+                alt = dist[u] + graph.getEdge(u, v)
+                if alt < dist[v]:
+                    dist[v] = alt
+                    prev[v] = u
+                    
+        return dist, prev
+    
+    #This code constructs the path
+    prev = dijkstra(graph, rootNode, targetNode)[1]
+    S = []
+    u = targetNode
+    while prev[u]: #backtrace the path from target vertex back to source vertex
+        S.append(u)
+        u = prev[u]
+    S.append(u)
+    S.reverse() #reverse the path to point from source to target
+    return S
+
+def getDistanceBetweenAirports(airportList, airportCode1, airportCode2):
+    """Returns the great circle distance between two airports"""
+    
+    def coordDist(latLong1, latLong2):
+        """Returns the great circle distance between two sets of coordinates"""
+        lat1 = math.radians(90-latLong1[0])
+        long1 = math.radians(latLong1[1])
+        lat2 = math.radians(90-latLong2[0])
+        long2 = math.radians(latLong2[1])
+
+        product1 = math.sin(lat1)*math.sin(lat2)*math.cos(long1-long2)
+        product2 = math.cos(lat1)*math.cos(lat2)
+
+        return round(math.acos(product1+product2)*6371)
+    
+    
+    airport1Coord = airportList[airportCode1][2]
+    airport2Coord = airportList[airportCode2][2]
+    
+    return coordDist(airport1Coord, airport2Coord)
+    
--- tests/test_ernestas_monkevicius_14493758_project.py
+++ tests/test_ernestas_monkevicius_14493758_project.py
@@ -5,19 +5,54 @@ Created on 3 Apr 2018
 '''
 import unittest
 from ernestas_monkevicius_14493758_project import data_structures
+from ernestas_monkevicius_14493758_project import utility
 
-#EXAMPLE TESTING
 class Test(unittest.TestCase):
+    
+    #Creating a test graph. The structure is just like the example in lecture notes.
+    testGraph = utility.Graph()
+
+    for i in "abcdefghij":
+        testGraph.setVertex(i)
         
-    #def test_invalid1(self):
-    #    t = Triangle(1, 2, 4)
-    #    self.assertTrue(t.classify() == "INVALID")
+    testGraph.setEdge('a', ['b', 'c', 'd', 'e', 'j'], [4,6,7,5,1])
+    testGraph.setEdge('b', 'aef', '498')
+    testGraph.setEdge('c', 'ad', '62')
+    testGraph.setEdge('d', 'acj', '723')
+    testGraph.setEdge('e', 'abfh', '5943')
+    testGraph.setEdge('f', 'beg', '847')
+    testGraph.setEdge('g', 'fi', [7,15])
+    testGraph.setEdge('h', 'ei', [3,12])
+    testGraph.setEdge('i', 'ghj', [15,12,9])
+    testGraph.setEdge('j', 'adi', '139')
         
-    def test_Currency(self):
-        currency = data_structures.Currency()
-        #Logic here...
-        self.assertTrue(True, "Success!!")
+    def test_Graph(self):
+        self.assertTrue(self.testGraph.getNeighbours('a') == {'b': 4, 'c': 6, 'd': 7, 'e': 5, 'j': 1})
+        self.assertTrue(list(self.testGraph.getVertices()) == ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+        self.assertTrue(self.testGraph.getEdge('a', 'b') == 4)
+        
+    def test_BFS(self):
+        self.assertTrue(utility.BFS(self.testGraph, 'c') == ['c', 'a', 'd', 'b', 'e', 'j', 'f', 'h', 'i', 'g'])
+    
+    def test_OptimalPath(self):
+        self.assertTrue(utility.optimalPath(self.testGraph, 'c', 'g') == ['c', 'a', 'e', 'f', 'g'])
         
         
+    airportList = {
+    'DUB' : ['Dublin', 'Ireland', (53.421333, -6.270075)],
+    'LHR' : ['Heathrow', 'United Kingdom', (51.4775, -0.461389)],
+    'JFK' : ['John F Kennedy Intl', 'United States', (40.639751, -73.778925)],
+    'AAL' : ['Aalborg', 'Denmark', (57.092789, 9.849164)],
+    'CDG' : ['Charles De Gaulle', 'France', (49.012779, 2.55)],
+    'SYD' : ['Sydney Intl', 'Australia', (-33.946111, 151.177222)]
+    }    
+    
+    def test_getDistanceBetweenAirports(self):
+        firstAirport = list(self.airportList.keys())[0]
+        answerList = []
+        for airport in self.airportList:
+            answerList.append([airport, utility.getDistanceBetweenAirports(self.airportList, firstAirport, airport)])
+        self.assertTrue(answerList == [['DUB', 0], ['LHR', 449], ['JFK', 5103], ['AAL', 1097], ['CDG', 785], ['SYD', 17215]])
+    
 if __name__ == '__main__':
     unittest.main()
